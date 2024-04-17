1658cfef34ed140a9d348bf6c14404721792f219
SENTHIL <123928930+hedge-hog-23@users.noreply.github.com>
2024-04-17 14:23:36 +0530

    Update moisture.ino

--- moisture.ino
+++ moisture.ino
@@ -8,7 +8,7 @@ void loop() {
   float moisture_percentage;
   int sensor_analog;
   sensor_analog = analogRead(sensor_pin);
-  moisture_percentage = (( (sensor_analog/1023.00) * 100 ));
+  moisture_percentage = ( 100 - ( (sensor_analog/1023.00) * 100 ) );
   Serial.print("Moisture Percentage = ");
   Serial.print(moisture_percentage);
   Serial.print("%\n\n");
